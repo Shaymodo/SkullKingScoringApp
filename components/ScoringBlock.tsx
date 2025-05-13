a9041d1d57297cdfb469dff572aed5ca1ce1ff1c
CJ Howell <ninjacal91@gmail.com>
2025-05-13 10:38:13 -0600

    added ScoringBlock to components

---
+++ components/ScoringBlock.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+import { Text, View } from "./Themed";
+
+export default function ScoreBlock(){
+    return(
+        <View>Testing</View>
+    );
+}
